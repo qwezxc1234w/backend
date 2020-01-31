3634c46a6d7e8e9692cc7ad235345a3ca5ab4798
Gleb Dudakov <scarawooo@gmail.com>
2020-01-31 19:32:25 +0700

    db + rooms

---
+++ src/main/java/ru/cft/shift/santa/exception/RoomIsFullException.java
@@ -0,0 +1,12 @@
+package ru.cft.shift.santa.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Если при обработке пользовательского запроса будет выброшено это исключение,
+ * то пользователю вернётся ответ со статусом 507 (INSUFFICIENT_STORAGE)
+ */
+@ResponseStatus(value = HttpStatus.INSUFFICIENT_STORAGE)
+public class RoomIsFullException extends RuntimeException {
+}
